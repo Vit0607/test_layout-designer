595eb4f47edae8e665ec3049f45fce81352b0d5a
Vitaly Yurkin <jvn9405@gmail.com>
2025-07-04 10:04:58 +0300

    Настройка .prettierrc, .vscode, eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,66 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
-
-export default tseslint.config([
-  globalIgnores(['dist']),
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import eslintReact from 'eslint-plugin-react';
+import eslintReactHooks from 'eslint-plugin-react-hooks';
+import eslintReactRefresh from 'eslint-plugin-react-refresh';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import { fixupConfigRules } from "@eslint/compat";
+export default tseslint.config({
+  plugins: {
+    '@typescript-eslint': tseslint.plugin,
+    'react-hooks': eslintReactHooks,
+    react: eslintReact,
+    'react-refresh': eslintReactRefresh,
+    prettier: prettierPlugin,
+  },
+},
   {
+    ignores: ['dist', 'node_modules', 'coverage', 'eslint.config.js'],
+  },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prefer-const': 'error',
+      'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
+      'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
+      'react/self-closing-comp': ['error', { component: true, html: true }],
+      'max-lines': ['warn', { max: 124 }],
+      'prettier/prettier': [
+        'warn',
+        {
+          endOfLine: 'auto'
+        }
+      ]
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
-    },
+    }
   },
-])
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2021
+      },
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.node.json'],
+      }
+    }
+  },
+  pluginJs.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+);
