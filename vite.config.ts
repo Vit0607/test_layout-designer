b1638381a72b2a5e37b806077c39c7d31701ea69
Vitaly Yurkin <jvn9405@gmail.com>
2025-07-04 12:27:52 +0300

    Создал структуру проекта и настроил алиасы

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@styles': '/src/styles',
+      '@types': '/src/types',
+      '@': '/src'
+    }
+  }
+});
